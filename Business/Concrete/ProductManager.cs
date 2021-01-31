ba68669a88b496b0a047f9d74826ee0ecdb2ab32
Uğur Cem Çergel <ugurcemcergel@gmail.com>
2021-01-31 14:42:46 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _ProductDal;
+        private InMemoryProductDal InMemoryProductDal;
+
+        public ProductManager(InMemoryProductDal InMemoryProductDal)
+        {
+            this.InMemoryProductDal = InMemoryProductDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş Kodları
+            //Örnek: Yetkisi var mı?
+            //Bütün kuralları geçitikten sonra alttaki kodu çalıştırıyor.
+            return _ProductDal.GetAll();
+            
+        }
+    }
+}
