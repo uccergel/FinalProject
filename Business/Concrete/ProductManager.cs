5c2ab477a3360d05dd3a8542f593680026ed1d82
Uğur Cem Çergel <ugurcemcergel@gmail.com>
2021-02-05 00:45:45 +0300

    05.02.2021 Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -11,20 +11,26 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _ProductDal;
-        private InMemoryProductDal InMemoryProductDal;
-
-        public ProductManager(InMemoryProductDal InMemoryProductDal)
+        public ProductManager(IProductDal productDal)
         {
-            this.InMemoryProductDal = InMemoryProductDal;
+            _ProductDal = productDal;
         }
-
         public List<Product> GetAll()
         {
             //İş Kodları
             //Örnek: Yetkisi var mı?
             //Bütün kuralları geçitikten sonra alttaki kodu çalıştırıyor.
             return _ProductDal.GetAll();
-            
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _ProductDal.GetAll(p=>p.CategoryId==id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _ProductDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
         }
     }
 }
